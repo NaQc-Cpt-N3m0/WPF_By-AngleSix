d1dfed7ac92def9372bd4cabbf9a88d989a91b34
Quân Đào <quanvdao.ee@gmail.com>
2024-04-19 14:17:02 +0700

    Check box gán vào TextBox

--- 001_The_Basic_UI/MainWindow.xaml.cs
+++ 001_The_Basic_UI/MainWindow.xaml.cs
@@ -28,8 +28,14 @@ namespace _001_The_Basic_UI
 
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
+            /// Gán các CheckBox với và bỏ check
             this.WeldCheckBox.IsChecked = this.AssemblyCheckBox.IsChecked = this.PlasmaCheckBox.IsChecked = this.LaserBox.IsChecked = this.PurchaseCheckBox.IsChecked =
                 this.LatheCheckBox.IsChecked = this.DrillCheckBox.IsChecked = this.FoldCheckBox.IsChecked = this.RollCheckbox.IsChecked = this.SawCheckBox.IsChecked = false;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.LenghtText.Text += ((CheckBox)sender).Content; // Ở đây Text của TextBox sẽ cộng bằng các conten được tích vào.
         }
     }
 }
